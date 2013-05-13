207a149e2d4423b61747f3f5ee7cd9b01e935fe3
dongyuwei <newdongyuwei@gmail.com>
2013-05-13 16:18:54 +0800

    update domain intro

--- nodejs-web-dev-demo/domain-error-test.js
+++ nodejs-web-dev-demo/domain-error-test.js
@@ -5,6 +5,19 @@ process.on('uncaughtException', function(err) {
     console.error('Caught exception: ', err);
 });
 
+try{
+    process.nextTick(function() {
+        setTimeout(function() { // simulating some various async stuff
+            fs.open('non-existent file', 'r', function(er, fd) {
+                if (er) throw er;
+            });
+        }, 100);
+    });
+}catch(e){
+    console.error(e);
+    consol.log('catch nothing!');
+}
+
 var d = domain.create();
 d.on('error', function(er) {
     console.error('Caught error!', er);
